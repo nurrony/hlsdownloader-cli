16314dc2d275f02979fbf5f52a242f964699c496
Nur Rony <pro.nmrony@gmail.com>
2017-11-28 21:01:04 +0600

    adds cli with alpha release note

---
+++ bin/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const message = `Lots of thanks for your interest. This module is under active development.
+Please check https://github.com/nmrony/hlsdownloader-cli for more details.`
+console.log()
+console.log(message)
+console.log()
