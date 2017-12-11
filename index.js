d43297310d1eecd08f9e0ba0c0c77a2e308500a1
Nur Rony <pro.nmrony@gmail.com>
2017-12-11 23:02:23 +0600

    added helper functions

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
 #!/usr/bin/env node
 import minimist from 'minimist'
+import HLSDownloader from 'hlsdownloader'
+
 import { createReadStream } from 'fs'
 import { version } from './../package.json'
 
+function prepareExtraParams(requestParams) {
+  const requestObject = {}
+  Object.keys(requestParams).map(paramName => {
+    try {
+      requestObject[paramName] = JSON.parse(requestParams[paramName])
+    } catch (exception) {
+      const error = new Error(`${paramName} is not a valid JSON string`)
+      error.name = 'ERR_MALFORM_JSON'
+      throw error
+    }
+  })
+
+  return requestObject
+}
+const commandHandlers = {
+  download(downloaderParams) {
+    //HLSDownloader.download({ playlistURL, destination })
+    console.log('testing', downloaderParams)
+  },
+  help() {
+    createReadStream('./help.txt').pipe(process.stdout)
+    return true
+  },
+  version() {
+    console.log('hlsdownloader cli version: ', version)
+    return true
+  }
+}
+
+function commandFlagPassed({ help, version }) {
+  const command = (help && 'help') || (version && 'version')
+  return command && commandHandlers[command]()
+}
+
+function availableCommandPassed(playlistURL) {
+  const commandList = Object.keys(commandHandlers)
+  return (playlistURL && commandList.indexOf(playlistURL) !== -1 && commandHandlers[playlistURL]())
+}
+
 const options = {
-  string: ['download-dir'],
+  string: ['destination'],
   boolean: ['version', 'help'],
   alias: {
-    d: 'download-directory',
+    d: 'destination',
     h: 'help',
     v: 'version'
   },
   default: {
-    'destination-directory': './',
+    destination: './',
     help: false,
     version: false
   }
@@ -20,17 +61,16 @@ const options = {
 
 const argv = minimist(process.argv.slice(2), options)
 
-function help () {
-  return createReadStream('./help.txt').pipe(process.stdout)
+function execute({ destination, d, version, v, help, h, _, ...restArgs }) {
+  const playlistURL = _.pop()
+  const commandExecuted = commandFlagPassed({ help, version }) || availableCommandPassed(playlistURL)
+
+  if (!commandExecuted && playlistURL) {
+    const requestObject = prepareExtraParams(restArgs)
+    commandHandlers['download']({ playlistURL, destination, ...requestObject })
+  } else {
+    commandHandlers['help']()
+  }
 }
 
-(function execute (argv) {
-  if (argv.h) {
-    return help()
-  }
-
-  if (argv.v) {
-    return console.log('hlsdownloader cli version: ', version)
-  }
-  // start download
-})(argv, undefined)
+execute(argv)
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,5 +2,8 @@ import { assert } from 'chai'
 
 describe('Array', () => {
   it('should start empty', () => {
+    var arr = []
+
+    assert.equal(arr.length, 0)
   })
 })
